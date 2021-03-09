72042fcb31617194fcb289d3acce1e52aa8e538c
Ersidas Baniulis <ersidasb@gmail.com>
2021-03-10 00:11:50 +0200

    Added ability to read/write files
    Added input file selection dialog
    Added option to output to file with custom name

--- AES_Algoritmas/AES_Algoritmas/MainWindow.xaml.cs
+++ AES_Algoritmas/AES_Algoritmas/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace AES_Algoritmas
 {
@@ -25,6 +27,11 @@ namespace AES_Algoritmas
             InitializeComponent();
             textBoxIV.MaxLength = 16;
             textBoxKey.MaxLength = 16;
+            radioButtonECB.IsChecked = true;
+            radioButtonCypherInputText.IsChecked = true;
+            radioButtonCypherOutputText.IsChecked = true;
+            radioButtonDecypherInputText.IsChecked = true;
+            radioButtonDecypherOutputText.IsChecked = true;
         }
 
         private void buttonEncrypt_Click(object sender, RoutedEventArgs e)
@@ -34,8 +41,32 @@ namespace AES_Algoritmas
                 mode = 2;
             try
             {
-                AES aes = new AES(textBoxKey.Text, textBoxIV.Text, mode);
-                textBoxCypherOutput.Text = aes.encrypt(textBoxCypherInput.Text);
+                string text;
+                AES aes;
+                aes = new AES(textBoxKey.Text, textBoxIV.Text, mode);
+                if (radioButtonCypherInputText.IsChecked == true)
+                {
+                    text = textBoxCypherInput.Text;
+                }
+                else
+                {
+                    text = File.ReadAllText(textBoxCypherFilePath.Text);
+                }
+
+                if(radioButtonCypherOutputText.IsChecked == true)
+                {
+                    textBoxCypherOutput.Text = aes.encrypt(text);
+                }
+                else
+                {
+                    if (!File.Exists($"{textBoxCypherFileName.Text}.txt") || File.Exists($"{textBoxCypherFileName.Text}.txt") && MessageBox.Show("File exists, do you want to overwrite it?", "File exists", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        StreamWriter sw = new StreamWriter($"{textBoxCypherFileName.Text}.txt");
+                        sw.WriteLine(aes.encrypt(text));
+                        sw.Close();
+                        MessageBox.Show("File created successfully");
+                    }
+                }
             }
             catch(Exception exc)
             {
@@ -50,13 +81,130 @@ namespace AES_Algoritmas
                 mode = 2;
             try
             {
+                string text;
                 AES aes = new AES(textBoxKey.Text, textBoxIV.Text, mode);
-                textBoxDecypherOutput.Text = aes.decrypt(textBoxDecypherInput.Text);
+                if (radioButtonDecypherInputText.IsChecked == true)
+                {
+                    text = textBoxDecypherInput.Text;
+                }
+                else
+                {
+                    text = File.ReadAllText(textBoxDecypherFilePath.Text);
+                }
+
+                if (radioButtonDecypherOutputText.IsChecked == true)
+                {
+                    textBoxDecypherOutput.Text = aes.decrypt(text);
+                }
+                else
+                {
+                    if (!File.Exists($"{textBoxDecypherFileName.Text}.txt") || File.Exists($"{textBoxDecypherFileName.Text}.txt") && MessageBox.Show("File exists, do you want to overwrite it?", "File exists", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        StreamWriter sw = new StreamWriter($"{textBoxDecypherFileName.Text}.txt");
+                        sw.WriteLine(aes.decrypt(text));
+                        sw.Close();
+                        MessageBox.Show("File created successfully");
+                    }
+                }
             }
             catch (Exception)
             {
                 MessageBox.Show("Decryption unsuccessful.");
             }
         }
+
+        //mode selection
+        private void radioButtonECB_Checked(object sender, RoutedEventArgs e)
+        {
+            labelIV.Visibility = Visibility.Collapsed;
+            textBoxIV.Visibility = Visibility.Collapsed;
+        }
+
+        private void radioButtonCBC_Checked(object sender, RoutedEventArgs e)
+        {
+            labelIV.Visibility = Visibility.Visible;
+            textBoxIV.Visibility = Visibility.Visible;
+        }
+
+        //Cypher input
+        private void radioButtonCypherInputText_Checked(object sender, RoutedEventArgs e)
+        {
+            textBoxCypherInput.Visibility = Visibility.Visible;
+            gridCypherInput.Visibility = Visibility.Collapsed;
+        }
+
+        private void radioButtonCypherInputFile_Checked(object sender, RoutedEventArgs e)
+        {
+            textBoxCypherInput.Visibility = Visibility.Collapsed;
+            gridCypherInput.Visibility = Visibility.Visible;
+        }
+
+        //Cypher output
+        private void radioButtonCypherOutputText_Checked(object sender, RoutedEventArgs e)
+        {
+            textBoxCypherOutput.Visibility = Visibility.Visible;
+            gridCypherOutput.Visibility = Visibility.Collapsed;
+        }
+
+        private void radioButtonCypherOutputFile_Checked(object sender, RoutedEventArgs e)
+        {
+            textBoxCypherOutput.Visibility = Visibility.Collapsed;
+            gridCypherOutput.Visibility = Visibility.Visible;
+        }
+
+        //Decypher input
+        private void radioButtonDecypherInputText_Checked(object sender, RoutedEventArgs e)
+        {
+            textBoxDecypherInput.Visibility = Visibility.Visible;
+            gridDecypherInput.Visibility = Visibility.Collapsed;
+        }
+
+        private void radioButtonDecypherInputFile_Checked(object sender, RoutedEventArgs e)
+        {
+            textBoxDecypherInput.Visibility = Visibility.Collapsed;
+            gridDecypherInput.Visibility = Visibility.Visible;
+        }
+
+        //Decypher output
+        private void radioButtonDecypherOutputText_Checked(object sender, RoutedEventArgs e)
+        {
+            textBoxDecypherOutput.Visibility = Visibility.Visible;
+            gridDecypherOutput.Visibility = Visibility.Collapsed;
+        }
+
+        private void radioButtonDecypherOutputFile_Checked(object sender, RoutedEventArgs e)
+        {
+            textBoxDecypherOutput.Visibility = Visibility.Collapsed;
+            gridDecypherOutput.Visibility = Visibility.Visible;
+        }
+
+        private void buttonCypherBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Filter = "Text Files|*.txt";
+            dialog.DefaultExt = ".txt";
+            Nullable<bool> dialogOK = dialog.ShowDialog();
+
+            if(dialogOK == true)
+            {
+                textBoxCypherFilePath.Text = dialog.FileName;
+            }
+        }
+
+        private void buttonDecypherBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Filter = "Text Files|*.txt";
+            dialog.DefaultExt = ".txt";
+            Nullable<bool> dialogOK = dialog.ShowDialog();
+
+            if (dialogOK == true)
+            {
+                textBoxDecypherFilePath.Text = dialog.FileName;
+            }
+        }
+
     }
 }
